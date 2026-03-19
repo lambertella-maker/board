2b430e1eae3cfbd2c61fed52c0fea37453752ba6
Ella Lambert <ellalambert@MacBook-Air-37.local>
2026-03-19 10:19:00 +0000

    update dashboards 2026-03-19 10:19

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mealplan-v1773915300';
+const CACHE = 'mealplan-v1773915540';
 const ASSETS = ['./mealplan.html', './ep6-burger.png', './y18-panang.png'];
 
 self.addEventListener('install', e => {
